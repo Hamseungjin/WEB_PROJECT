19d794c042229d4a34c1b2ee954365b3967ca2a9
banxzxx <jwbang1643@gmail.com>
2024-06-11 11:30:48 +0900

    put profiles & change btn path

--- app.py
+++ app.py
@@ -11,9 +11,12 @@ from chat import get_response
 
 app=Flask(__name__)
 """ --------------------------------------------------------------------------------------------------------- """
-client_id = os.getenv("client_id")
-client_secret = os.getenv("client_secret")
-app.secret_key = os.getenv("secret_key")
+# client_id = os.getenv("client_id")
+# client_secret = os.getenv("client_secret")
+# app.secret_key = os.getenv("secret_key")
+client_id = '36b258fc1e66468eb7b030418e2363c0'
+client_secret = 'e1dd5b9ba7884a8ea1480c474e579200'
+app.secret_key = 'hsjking0403@naver.com'
 """ --------------------------------------------------------------------------------------------------------- """
 
 # MongoDB connection setup
@@ -147,7 +150,7 @@ def home_page():
         return render_template("unauthorized.html", message="This feature is available for paid subscribers only.")
 
     # Proceed to render the home page for subscribers
-    return flask.render_template("landing.html")
+    return flask.render_template("main.html")
 
 
 @app.route('/playlists')
@@ -541,6 +544,10 @@ def checkLayout():
 @app.route("/check")
 def check():
     return render_template("playlist.html")
+
+@app.route("/song")
+def songlist():
+    return render_template("songlist.html")
 
 
 if __name__ == '__main__':
